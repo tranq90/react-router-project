3f208c46bdfb981c53dfdbc934585d41cd26b91b
Quan Tran <tran.q90@gmail.com>
2021-04-28 22:44:03 -0700

    params

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,5 +1,20 @@
-const AllQuote = () => {
-  return <h1>All Quotes</h1>;
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+  {
+    id: "q1",
+    author: "Max",
+    text: "Learning React is fun!",
+  },
+  {
+    id: "q2",
+    author: "Quan",
+    text: "Learning React is great!",
+  },
+];
+
+const AllQuotes = () => {
+  return <QuoteList quotes={DUMMY_QUOTES} />;
 };
 
-export default AllQuote;
+export default AllQuotes;
--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,6 +1,12 @@
+import QuoteForm from '../components/quotes/QuoteForm'
+
 const NewQuote = () => {
+    const addQuoteHandler = quoteData => {
+        console.log(quoteData);
+    }
+
     return(
-        <h1>New Quote</h1>
+        <QuoteForm onAddQuote={addQuoteHandler} />
     );
 }
 
